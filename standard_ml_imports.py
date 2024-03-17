316ceef0122d7be5dba2941909393dce1d8f263a
Mr Nosaj <131139729+MrNosaj@users.noreply.github.com>
2024-03-18 08:52:10 +1100

    Create standard_ml_imports.py
    Add standard ML import block

---
+++ standard_ml_imports.py
@@ -0,0 +1,22 @@
+# Data manipulation
+import numpy as np
+import pandas as pd
+
+# Data visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Machine learning - Preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+
+# Machine learning - Models
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+
+# Machine learning - Model evaluation
+from sklearn.metrics import mean_squared_error, r2_score
+
+# To handle datasets
+from sklearn.datasets import load_iris, load_boston
